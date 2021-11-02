64429285b00701a5b38a3be68dbfe8fe08702106
wandieinnocents <wandieinnocent2@gmail.com>
2021-11-02 15:03:43 +0300

    Add login,logout and fetch authenticated user

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -35,12 +35,21 @@ return [
     |
     */
 
+
     'guards' => [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],  
+
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
+
+
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 /*
@@ -20,9 +22,58 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// AUTHENTICATION API
+
+// create user route
+Route::post('user-create',function (Request $request){
+
+    User::create(
+        [
+            'name' => 'qwandie',
+            'email' => 'qwandieinnocent2@gmail.com',
+            'password' => Hash::make('wandie22')
+            ]
+     );
+});
+
+//login a user
+
+Route::post('login', function (Request $request){
+
+    $credentials = request()->only(['email','password']);
+
+    $token = auth()->attempt($credentials);
+    return $token;
+
+});
 
 
-// get postsPostApiControllerr
+//return logged in user
+// user should be logged
+// Testing with postaman use  Headers ,
+//add key content-type : value : application/json ..
+//add other key : Authorization value : Bearer + token (of logged in user)
+Route::middleware('auth:api')->get('/me',function (){
+
+    return response()->json(auth()->user());
+
+});
+
+// logout user
+// user should be logged in
+// Testing with postaman use  Headers ,
+//add key content-type : value : application/json ..
+//add other key : Authorization value : Bearer + token (of logged in user)
+Route::post('logout', function (Request $request){
+
+    auth()->logout();
+    return response()->json(['message' => 'Successfully logged out']);
+
+
+});
+
+
+// Posts Api - Index,Store,Update,Delete
 Route::resource('/posts','App\Http\Controllers\Api\PostApiControllerr');
 
 
