cd0ebda460b26e7efa3c8bfdb4a39963a0cd976a
wandieinnocents <wandieinnocent2@gmail.com>
2021-11-02 16:40:58 +0300

    Add login responses

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -10,26 +10,50 @@ use Illuminate\Support\Facades\Hash;
 class AuthApiController extends Controller
 {
     //register api
-    public function register(){
-        User::create(
-            [
-                'name' => 'wwandie',
-                'email' => 'wwandieinnocent2@gmail.com',
-                'password' => Hash::make('wandie22')
-                ]
-         );
+    public function register(Request $request){
+
+         $user = User::create($request->all());
+
+        return response()->json([
+            "message" => "User created successfuly"
+           ],201);
     }
 
     // login api
+     public function login(){
+         $credentials = request()->only(['email','password']);
+         $token = auth()->attempt($credentials);
 
-    public function login(){
+        //  response
+         return response()->json([
+            'status' => 200,
+            "message" => "User Logged in successfuly",
+            "data" => $credentials,
+            "access_token" => $token,
+            "token_type" => 'bearer',
+            "expires_in" => auth()->factory()->getTTL() * 60
 
-        $credentials = request()->only(['email','password']);
+           ]);
 
-        $token = auth()->attempt($credentials);
-        return $token;
+        //  return $token;
 
     }
+
+    public function me(){
+        // Testing with postaman use  Headers ,
+        //add key content-type : value : application/json ..
+        //add other key : Authorization value : Bearer + token (of logged in user)
+        return response()->json(auth()->user());
+
+
+    }
+
+    public function logout(){
+
+        auth()->logout();
+       return response()->json(['message' => 'Successfully logged out']);
+    }
+
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// AUTHENTICATION API ROUTES
 // create user
 Route::post('register','App\Http\Controllers\Api\AuthApiController@register');
 
@@ -29,32 +30,14 @@ Route::post('register','App\Http\Controllers\Api\AuthApiController@register');
 Route::post('login','App\Http\Controllers\Api\AuthApiController@login');
 
 // Return authenticated user
-// Testing with postaman use  Headers ,
-//add key content-type : value : application/json ..
-//add other key : Authorization value : Bearer + token (of logged in user)
-Route::post('login','App\Http\Controllers\Api\AuthApiController@login');
+Route::middleware('auth:api')->get('/me','App\Http\Controllers\Api\AuthApiController@me');
 
-Route::middleware('auth:api')->get('/me',function (){
-
-    return response()->json(auth()->user());
-
-});
-
-// logout user
-// user should be logged in
-// Testing with postaman use  Headers ,
-//add key content-type : value : application/json ..
-//add other key : Authorization value : Bearer + token (of logged in user)
-Route::post('logout', function (Request $request){
-
-    auth()->logout();
-    return response()->json(['message' => 'Successfully logged out']);
+// Logout user api
+Route::post('logout','App\Http\Controllers\Api\AuthApiController@logout');
 
 
-});
 
-
-// Posts Api - Index,Store,Update,Delete
+// POSTS API ROUTES  - Index,Store,Update,Delete
 Route::resource('/posts','App\Http\Controllers\Api\PostApiControllerr');
 
 
